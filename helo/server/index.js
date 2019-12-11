4594aa8d85e6eec7114f16e046e51fd602fd5b18
spiltmiruku <spiltmiruku@gmail.com>
2019-12-11 11:09:38 -0700

    Updated server file with minimum viable product and bonus features

--- helo/server/index.js
+++ helo/server/index.js
@@ -3,17 +3,18 @@ const express = require('express'),
       massive = require('massive'),
       session = require('express-session'),
       gradient = require('gradient-string'),
+      authCtrl = require('./authController'),
       ctrl = require('./controller'),
       { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env,
       app = express();
 
 app.use(express.json());
-// app.use(session({
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {maxAge: 1000 * 60 * 60},
-//     secret: SESSION_SECRET
-// }))
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 60},
+    secret: SESSION_SECRET
+}))
 
 massive(CONNECTION_STRING)
     .then(db => {
@@ -21,6 +22,17 @@ massive(CONNECTION_STRING)
     console.log(gradient.retro('DB Cooper is watching'))
 })
 .catch(err => console.log(gradient.vice(err, `Lost contact with DB Cooper`)));
+
+// Minimum Viable Product
+
+app.post('/api/auth/register', authCtrl.login);
+app.post('/api/auth/login', authCtrl.register);
+
+// Bonus features
+
+// app.get('/api/posts/:userid');
+// app.post('/api/post/:userid');
+// app.get('/api/post/:postid');
 
 
 const port = SERVER_PORT;
--- helo/src/Components/Auth/Auth.js
+++ helo/src/Components/Auth/Auth.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import axios from 'axios';
+// import {getUser} from '../redux/reducer';
 
 class Auth extends Component {
     constructor(){
         super();
         this.state = {
-
+            username: '',
+            password: ''
         }
     }
+
+
+    
     render(){
         return(
             <div>Auth</div>
