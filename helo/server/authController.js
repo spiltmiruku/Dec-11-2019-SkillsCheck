4c4d2c7caa0aedb965b73374189a3bd3a18a7de6
spiltmiruku <spiltmiruku@gmail.com>
2019-12-11 11:21:36 -0700

    Setting up store and reducer for redux

--- helo/server/authController.js
+++ helo/server/authController.js
@@ -1,5 +1,12 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 module.exports = {
-    
+    login: async(req, res) => {
+        console.log(req);
+        const { username, password } = req.body;
+        const { session } = req;
+        const db = req.app.get('db');
+
+        let user = await db.check_user(username)
+    }
 }
