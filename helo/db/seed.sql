5372e50e95407d3be525d670df20a56b829a6677
spiltmiruku <spiltmiruku@gmail.com>
2019-12-11 10:48:10 -0700

    Setting up routes and db files

--- helo/db/seed.sql
+++ helo/db/seed.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS users_sc (
     password VARCHAR(20),
     profile_pic text
 );
+
+CREATE TABLE IF NOT EXISTS posts_sc (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(45),
+    img TEXT,
+    content TEXT,
+    author_id INTEGER REFERENCES users_sc(id)
+);
