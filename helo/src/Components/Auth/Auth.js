fd14ca6dd8320210c3e4f92777626793c55ae3e2
spiltmiruku <spiltmiruku@gmail.com>
2019-12-11 09:39:20 -0700

    Creating basic files (Components)

---
+++ helo/src/Components/Auth/Auth.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Auth extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>Auth</div>
+        )
+    }
+}
+
+
+export default Auth;
---
+++ helo/src/Components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>Nav</div>
+        )
+    }
+}
+
+export default Nav;
