14c9b2ad1a261aae3902576d4f5078e60b52ad98
spiltmiruku <spiltmiruku@gmail.com>
2019-12-11 11:35:03 -0700

    Setting up controller file for registration, login, and logout

--- helo/server/authController.js
+++ helo/server/authController.js
@@ -7,6 +7,48 @@ module.exports = {
         const { session } = req;
         const db = req.app.get('db');
 
-        let user = await db.check_user(username)
-    }
-}
+        let user = await db.check_user(username);
+        user = user[0];
+        if(!user){
+            return res.status(400).send('Username not found')
+        }
+        const authenticated = bcrypt.compareSync(password, user.user_password);
+        if(authenticated){
+            delete user.user_password;
+            session.user = user;
+            res.status(202).send(session.user);
+        } else {
+            res.status(401).send('Incorrect Password')
+        }
+    },
+    register: async(req, res) => {
+        const { username, password } = req.body;
+        const { session } = req;
+        const db = req.app.get('db');
+
+        let user = await db.check_user(username);
+        user = user[0];
+        if(user){
+            return res.status(400).send('Username already exists')
+        }
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        let newUser = await db.register_user({ username, hash });
+        newUser = newUser[0];
+        session.user = newUser;
+        res.status(201).send(session.user);
+    },
+    logout: (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    },
+    getUser: (req, res) => {
+        if(req.session.user){
+            res.status(200).send(req.session.user);
+        } else {
+            res.status(200).send('');
+        }
+    },
+
+
+};
